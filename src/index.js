f88981ae602de9b3c68af060196e9e0beeb50835
Alucard <xavier_ortega98@hotmail.com>
2021-07-07 21:15:49 -0500

    Seccion 10 terminada / faltan fixes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 // // import { CallbackHook } from './components/06-memos/CallbackHook';
 // import { Padre } from './components/07-tarea-memo/Padre';
 // // import { MemoHook } from './components/06-memos/MemoHook';
@@ -14,10 +14,10 @@
 // // import { SimpleForm } from './components/02-useEffect/SimpleForm';
 // // import { HookApp } from './HookApp';
 
+import { TodoApp } from './components/08-useReducer/TodoApp';
 
-// ReactDOM.render(
-//     <Padre />,
-//   document.getElementById('root')
-// );
+ReactDOM.render(
+    <TodoApp/>,
+  document.getElementById('root')
+);
 
-import './components/08-useReducer/intro-reducer';
